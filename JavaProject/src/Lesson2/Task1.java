2a448d8fd10a9a647f5fa8724096cfc8a523eb86
JuliaJJacob <j.j.dignus@gmail.com>
2022-12-24 16:28:35 +0200

    Homework1

--- JavaProject/src/Lesson2/Task1.java
+++ JavaProject/src/Lesson2/Task1.java
@@ -1,3 +1,5 @@
+package Lesson2;
+
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("\t\tРУБАИ\n");
---
+++ JavaProject/src/Lesson2/homework/Homework_l2.java
@@ -0,0 +1,64 @@
+package Lesson2.homework;
+
+public class Homework_l2 {
+    public static void main(String[] args) {
+
+        // HW 1.1
+        byte myByte1 = 126;
+        byte myByte2 = -120;
+
+        short myShort1 = 3333;
+        short myShort2 = -8888;
+
+        int myInt1 = 12364563;
+        int myInt2 = 33332222;
+
+        long myLong1 = 6666666666666666666L;
+        long myLong2 = -7777777777777777777L;
+
+        System.out.println("\nHW 1.1");
+        System.out.println("myByte1 = " + myByte1);
+        System.out.println("myByte2 = " + myByte2);
+        System.out.println("myShort1 = " + myShort1);
+        System.out.println("myShort2 = " + myShort2);
+        System.out.println("myInt1 = " + myInt1);
+        System.out.println("myInt2 = " + myInt2);
+        System.out.println("myLong1 = " + myLong1);
+        System.out.println("myLong2 = " + myLong2);
+
+        // HW 1.2
+
+        float myFloat1 = 3.33f;
+        float myFloat2 = -3.33f;
+
+        double myDouble1 = 4.44;
+        double myDouble2 = -4.44;
+
+        boolean myBool1 = true;
+        boolean myBool2 = false;
+
+        System.out.println("\nHW 1.2");
+        System.out.println("myFloat1 = " + myFloat1);
+        System.out.println("myFloat2 = " + myFloat2);
+        System.out.println("myDouble1 = " + myDouble1);
+        System.out.println("myDouble2 = " + myDouble2);
+        System.out.println("myBool1 = " + myBool1);
+        System.out.println("myBool2 = " + myBool2);
+
+        // HW 1.3
+
+        char a = 'a';
+        char b = 'F';
+        char c = 33;
+        char d = '\u5566';
+        char e = '\u2192';
+
+        System.out.println("\nHW 1.3");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+
+    }
+}
