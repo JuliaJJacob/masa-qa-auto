a2f7c72bd316f7a1b8efceb18f05749d12398929
JuliaJJacob <j.j.dignus@gmail.com>
2022-12-24 20:26:58 +0200

    Lesson 3 - 1st Commit

---
+++ JavaProject/src/Lesson3/UserInput.java
@@ -0,0 +1,29 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+
+        String name;
+
+        int age;
+        double grade;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        name = input.nextLine();
+        System.out.println("Enter your age");
+        age = input.nextInt();
+        System.out.println("Enter your grade");
+        grade = input.nextDouble();
+
+        System.out.println("name = " + name);
+        System.out.println("name = " + age);
+        System.out.println("grade = " + grade);
+
+        System.out.println("Fin");
+
+    }
+}
