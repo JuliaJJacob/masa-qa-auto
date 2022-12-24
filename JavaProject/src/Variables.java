d5a66c6e8342f8f58b9489c5f9d1ce8ed055ab0c
JuliaJJacob <j.j.dignus@gmail.com>
2022-12-24 14:43:37 +0200

    Add Variables

---
+++ JavaProject/src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte myByte;
+        myByte = 4;
+        myByte = -128;
+        short myShort = 2343;
+        int myInt = 12121212;
+        long myLong = 132445778866868L;
+        System.out.println("myShort = " + myShort);
+
+        float myFloat = 4.56f;
+        double myDouble = 4.56;
+
+        boolean isTrue = true;
+
+        char myChar = 't';
+        myChar = '\u3453';
+        System.out.println(myChar);
+
+
+    }
+}
